7ebb22c38d05a9143b8b13442063774f07583737
Prathamesh Sawant <pratsawant1998@gmail.com>
2026-06-17 02:44:56 +0100

    Switch to publishable Supabase key via env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nvptwezhkjwybzynxvdp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cHR3ZWpra3p3eWJ6eW54dmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NDk0NDYsImV4cCI6MjA1MjQyNTQ0Nn0.5i2-8GcWzP3rO2d16TQO2pMm71UeJ19lC2z8k39sQw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
-
